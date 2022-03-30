908cb712888e48cfc1eb50e36a420e26335508de
HibariSan <97366901+HibariSan@users.noreply.github.com>
2022-03-30 21:42:43 +0000

    estructura básica funcionando

--- sokoban.py
+++ sokoban.py
@@ -1,1 +1,21 @@
-print ("hola")
+class Sokoban:
+  """
+  0_personaje
+  1_espacio
+  2_caja
+  3_pared
+  4_metas
+  5-persoaje_meta
+  6_caja_meta
+  """
+  
+  mapa=[
+    [3,3,3,3,3],
+    [3,1,1,1,1,3],
+    [3,1,1,0,1,3],
+    [3,1,1,1,1,3],
+    [3,3,3,3,3,3],
+  ]
+
+juego=Sokoban()
+print(juego.mapa)
